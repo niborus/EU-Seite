6a0b15348402b62e594c646374e0b99e15f05fcc
Robin <niborus.management@gmail.com>
2021-03-01 17:06:13 +0100

    Send request for count and check favorite-status on start.

--- static/frontend_script_every_site.js
+++ static/frontend_script_every_site.js
@@ -1,3 +1,13 @@
+function site_name() {
+    if (window.location.pathname === '/') {
+        return 'index';
+    } else {
+        const split_path = window.location.pathname.split('/');
+        const file_name = split_path[split_path.length - 1];
+        return file_name.split('.').slice(0, -1).join('.');
+    }
+}
+
 function connect_to_database_with_body(request, data= {}) {
     data["ref_site"] = window.location.pathname;
     request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
@@ -9,17 +19,42 @@ function connect_to_database_without_body(request) {
 }
 
 function send_fav_to_database(request, method) {
-    request.open(method, '/check');
-    const split_path = window.location.pathname.split('/');
-    const file_name = split_path[split_path.length - 1];
+    request.open(method, '/favorites');
     const data = {
-        'site_name': file_name.split('.').slice(0, -1).join('.'),
+        'site_name': site_name(),
     }
     connect_to_database_with_body(request, data);
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
     const star = document.getElementById("star_button");
+
+    function get_fav_from_database() {
+        const request = new XMLHttpRequest();
+        request.onload = function onload() {
+            if (request.status < 300) {
+                console.log(request.response);
+                console.log(site_name());
+                if (site_name() in request.response) {
+                    star.innerText = '\u2605';
+                }
+            }
+        }
+        request.open('GET', '/favorites');
+        request.responseType = 'json';
+        connect_to_database_without_body(request);
+    }
+    get_fav_from_database();
+
+    function post_access_count() {
+        const request = new XMLHttpRequest();
+        request.open('POST', '/access-count');
+        var data = {
+            'site_name': site_name()
+        }
+        connect_to_database_with_body(request, data);
+    }
+    post_access_count()
 
     function post_fav_to_database() {
         const request = new XMLHttpRequest();
