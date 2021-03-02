d47106322a84abf6c1b9e89d34d18ee52399c946
Robin <niborus.management@gmail.com>
2021-03-02 15:35:17 +0100

    Showing favorites and most-visited sites on homepage.

--- static/frontend_script_index.js
+++ static/frontend_script_index.js
@@ -3,9 +3,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
         const request = new XMLHttpRequest();
         request.onload = function onload() {
             if (request.status < 300) {
-                if (site_name() in request.response) {
-                    // TODO: Create fav table
-                }
+                let fav_new_inner_html = "";
+                request.response.forEach(function (site) {
+                    fav_new_inner_html += `<li><a href='${site['site_name']}.html'>${site['titel']}</a></li>`;
+                })
+                document.getElementById("fav_list").innerHTML = fav_new_inner_html;
             }
         }
         request.open('GET', '/favorites');
@@ -16,9 +18,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
         const request = new XMLHttpRequest();
         request.onload = function onload() {
             if (request.status < 300) {
-                if (site_name() in request.response) {
-                    // TODO: Create most visited sites table
-                }
+                let visited_new_inner_html = "";
+                let most_visited = request.response;
+                most_visited.sort((site1, site2) => { return site2['called'] - site1['called']; });
+                most_visited.forEach(function (site) {
+                    visited_new_inner_html += `<li><a href='${site['site_name']}.html'>${site['titel']}</a></li>`;
+                })
+                document.getElementById("visited_list").innerHTML = visited_new_inner_html;
             }
         }
         request.open('GET', '/access-count');
